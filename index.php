b464476de71a3f39cec86c52598e23b24c26fdb5
AllenPartain <119446724+AllenPartain@users.noreply.github.com>
2023-10-20 19:32:41 +0000

    Allen

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   </head>
   <body>
     <?php echo '<p>Hello World</p>'; ?> 
-First Attempt 3: I think we have it.
+First Attempt 3: I think we have it. Allen
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
   </body>
 </html>
